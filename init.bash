8697f6aaa774d676980a98a6d405362b2e9fe2ff
ichi_taro3 <taro3.ichi@gmail.com>
2020-07-22 08:22:10 +0900

    copy for bash completion

---
+++ init.bash
@@ -0,0 +1,11 @@
+function fzfcd {
+  target=$1
+  maxdepth=$2
+  if [ -z ${target} ]; then
+    target="."
+  fi
+  moveto=$(find ${target} -type d -maxdepth ${maxdepth:=4} | sed '/\.git/d' |fzf --select-1 --exit-0 )
+  cd ${moveto}
+}
+
+alias ${MYCD:=zd}=fzfcd
